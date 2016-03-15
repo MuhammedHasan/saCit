7ac8b5883220d29a02f038b1145da976036e2b95
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-03-15 13:29:07 +0200

    little update in data

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from paper import *
 import json
 
 
-f = open('pselected.json')
-j = json.load(f)
+result_f = open('result', 'a')
+error_f = open('error-log', 'a')
 
-for i in j:
-    p = Paper(i['doi'])
-    result_f = open('result', 'a')
-    error_f = open('error-log', 'a')
+
+for i in open('selected_ids.json'):
+    paper_id = i.strip()
     try:
+        p = Paper(paper_id)
         result_f.write(str(
-            '%s\n' % str({i['doi']: p.get_centiment_with_paperid()})
+            '%s\n' % str({paper_id: p.get_centiment_with_paperid()})
         ))
     except Exception as e:
-        error_f.write('%s\t%s\n' % (i['doi'], e.message))
+        error_f.write('%s\t%s\n' % (paper_id, e.message))
         continue
