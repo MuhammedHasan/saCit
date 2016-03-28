620d3992f5c31c66a01459dc4cf0d4bc76d4b6f8
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-03-28 18:47:29 +0300

    paralized

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from pymongo import MongoClient
 from paper import *
 import json
+import multiprocessing
+from joblib import Parallel, delayed
+import time
+import ast
+
+paper_ids = set([i.strip() for i in open('selected_ids.json')])
+
+result_f = open('result')
+paper_ids = paper_ids.difference(
+    [ast.literal_eval(i).keys()[0] for i in result_f])
+result_f.close()
+
+result_f = open('error-log')
+paper_ids = paper_ids.difference([i.split('\t')[0] for i in result_f])
+result_f.close()
+
+paper_ids = list(paper_ids)
+
+# def job(paper_id):
+#     result_f = open('result', 'a')
+#     error_f = open('error-log', 'a')
+#     p = Paper(paper_id)
+#     result_f.write(str(
+#         '%s\n' % str({paper_id: p.get_centiment_with_paperid()})
+#     ))
 
 
-result_f = open('result', 'a')
-error_f = open('error-log', 'a')
-
-
-for i in open('selected_ids.json'):
-    paper_id = i.strip()
+def job(paper_id):
+    result_f = open('result', 'a')
+    error_f = open('error-log', 'a')
     try:
         p = Paper(paper_id)
         result_f.write(str(
@@ -16,4 +38,7 @@ for i in open('selected_ids.json'):
         ))
     except Exception as e:
         error_f.write('%s\t%s\n' % (paper_id, e.message))
-        continue
+
+
+num_cores = multiprocessing.cpu_count()
+Parallel(n_jobs=num_cores)(delayed(job)(i) for i in paper_ids[:100])
--- paper/extract.py
+++ paper/extract.py
@@ -1,20 +1,26 @@
 import subprocess
 import settings
+import uuid
+import os
 
 
 def extract_all(paper_text):
     '''Extract all from text file and return string in xml format'''
+    filename = _save_into_docker(paper_text)
+
     proc = subprocess.Popen(
         [
             'docker', 'exec', 'pars',
             'local/parscit/bin/citeExtract.pl',
             '-m', 'extract_all',
             settings.PARSCIT_CONTAINER_DATA_VOLUME_LOCATION_FOR_CONTAINER +
-            '/' + _save_into_docker(paper_text)
+            '/' + filename + '.txt'
         ],
         stdout=subprocess.PIPE)
 
     (output, errors) = proc.communicate()
+
+    _delete_files(filename)
 
     if errors:
         raise RuntimeError(errors)
@@ -23,10 +29,17 @@ def extract_all(paper_text):
 
 
 def _save_into_docker(paper_text):
+    filename = str(uuid.uuid4())
     paper_file = open(
         settings.PARSCIT_CONTAINER_DATA_VOLUME_LOCATION_FOR_HOST + '/' +
-        'paper_text.txt', 'w'
+        filename + '.txt', 'w'
     )
     paper_file.write(paper_text.encode('utf-8'))
+    return filename
 
-    return 'paper_text.txt'
+
+def _delete_files(filename):
+    filepath = settings.PARSCIT_CONTAINER_DATA_VOLUME_LOCATION_FOR_HOST + '/' + filename
+    os.remove(filepath + '.txt')
+    os.remove(filepath + '.body')
+    os.remove(filepath + '.cite')
